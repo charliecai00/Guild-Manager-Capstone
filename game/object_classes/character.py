0a2f05898b6d4badc4fa2392caa9674a552b4b20
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2022-10-02 23:30:47 -0400

    Create character.py
    parent class for Adventurers and Enemies

---
+++ game/object_classes/character.py
@@ -0,0 +1,11 @@
+class Character:
+    def __init__(self) -> None:
+            self.stats = {
+                "STR": 0,
+                "CON": 0,
+                "DEX": 0,
+                "WIS": 0,
+                "INT": 0,
+                "CHA": 0,
+            }
+            self.attributes = []
