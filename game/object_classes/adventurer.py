f7f901a7b9a49b657a369004a0dd5fa92bae6967
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2022-10-02 23:36:46 -0400

    Update adventurer.py
    Made adventurer a child class of character

--- game/object_classes/adventurer.py
+++ game/object_classes/adventurer.py
@@ -1,14 +1,8 @@
 
+from game.object_classes.character import Character
 
-class Adventurer:
+class Adventurer(Character):
     def __init__(self) -> None:
-        self.stats = {
-            "STR": 0,
-            "CON": 0,
-            "DEX": 0,
-            "WIS": 0,
-            "INT": 0,
-            "CHA": 0,
-        }
+        Character.__init__(self)
         self.items = []
-        self.attributes = []
+    
