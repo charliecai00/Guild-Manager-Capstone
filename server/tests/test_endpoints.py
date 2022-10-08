9b9580ea87625987d46d34c18eb41e125c8b9f40
Charlie Cai <lc4181@nyu.edu>
2022-10-07 22:53:40 -0400

    test

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -1,11 +1,19 @@
-# import pytest
-# import server.endpoints as ep
 
-# TEST_CLIENT = ep.app.test_client()
+import pytest
 
-# def test_hello():
-#     resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
-#     assert isinstance(resp_json[ep.MESSAGE], str)
+import server.endpoints as ep
+
+TEST_CLIENT = ep.app.test_client()
+
+TEST_CHAR_TYPE = 'Warrior'
+
+
+def test_hello():
+    """
+    See if Hello works.
+    """
+    resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
+    assert isinstance(resp_json[ep.MESSAGE], str)
 
 # def test_command_list():
 #     res = TEST_CLIENT.get(ep.command_list).get_json()
@@ -15,5 +23,5 @@
 #     res = TEST_CLIENT.get(ep.input).get_json()
 #     assert isinstance(res[ep.MESSAGE],str)
 
-def test_dummy():
-    assert True
+# def test_dummy():
+#     assert True
