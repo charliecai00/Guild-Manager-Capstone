4e2dc046a8bcae4903c08daf54a7d0761ccb5d10
Jorge-A-Velasquez <jav494@nyu.edu>
2022-10-10 16:57:33 -0400

    Finished working prototype

--- game/game_object.py
+++ game/game_object.py
@@ -1,10 +1,43 @@
 
+from object_classes.challenge import Challenge
 from object_classes.guild import Guild
 
 
 class Game:
     def __init__(self):
         self.guild = Guild()
+        self.full_hero_list = []
+        self.full_quest_list = []
 
     def Get_Heros(self):
-        return self.guild.Find_Heros()
+        hero_list = self.guild.Find_Heros()
+        self.full_hero_list.extend(hero_list)
+        return hero_list
+
+    def Find_Hero(self, name):
+        for hero in self.full_hero_list:
+            if hero.name == name:
+                return hero
+
+    def Hire_Hero(self, name):
+        hero = self.Find_Hero(name)
+        return self.guild.Hire_Hero(hero)
+
+    def Guild_Status(self):
+        return self.guild
+
+    def Add_Party(self, name, list):
+        self.guild.Form_Party(list, name)
+
+    def Get_Quest(self):
+        new_quest = Challenge('STR')
+        self.full_quest_list.append(new_quest)
+        return new_quest
+
+    def Find_Quest(self, name):
+        for quest in self.full_quest_list:
+            if quest.name == name:
+                return quest
+
+    def Do_Quest(self, quest_name, party_name):
+        self.guild.Send_Quest(party_name, self.Find_Quest(quest_name))
--- game/object_classes/challenge.py
+++ game/object_classes/challenge.py
@@ -1,10 +1,17 @@
 
+import object_classes.static_consts as sc
+
 class Challenge:
-    def __init__(self) -> None:
-        self.test_skill = None
+    def __init__(self, skill=None) -> None:
+        self.test_skill = skill
+        self.name = str(sc.QUEST_ID)
+        sc.QUEST_ID += 1
 
     def Get_Skill(self):
         return self.test_skill
 
     def Set_Skill(self, new_skill):
         self.test_skill = new_skill
+
+    def __repr__(self) -> str:
+        return "Name: {}, Skill: {}".format(self.name, self.test_skill)
