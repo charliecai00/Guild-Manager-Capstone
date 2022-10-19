766e73046528595ac0a8ada21c84dcc1171efbb8
Charlie Cai <lc4181@nyu.edu>
2022-10-19 15:30:20 -0400

    Update guild.py

--- game/object_classes/guild.py
+++ game/object_classes/guild.py
@@ -1,6 +1,6 @@
 
-from object_classes.hero import Hero
-from object_classes.party import Party
+from game.object_classes.hero import Hero
+from game.object_classes.party import Party
 
 
 class Guild:
