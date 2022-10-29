25b9b097a5d393aa56d9ae13acdb998553c5b662
Charlie Cai <lc4181@nyu.edu>
2022-10-29 19:08:02 -0400

    update

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,5 +1,5 @@
-from flask import Flask  # , request
-from flask_restx import Resource, Api  # , fields
+from flask import Flask, request
+from flask_restx import Resource, Api, fields
 # import werkzeug.exceptions as wz
 
 app = Flask(__name__)
@@ -23,8 +23,23 @@ class command_list(Resource):
             "Do_Quest"
         ]}
 
+expected_input = api.model('expected_input', {
+    "Type": fields.String(default="Add_To_Party", required=True),
+    "Data": fields.String(default="specific data goes here", required=True)
+})
+
 @api.route((INPUT))
 class input(Resource):
     # Input takes any kind of JSON for whatever command
-    def get(self):
-        return {MESSAGE: "test"}
+    @api.expect(expected_input)
+    def post(self):
+        print(f'{request.json=}')
+        type = request.json["Type"]
+        if (type == "Add_To_Party"):
+            return {MESSAGE: "This is add to party"}
+        elif (type == "Do_Quest"):
+            pass
+        elif (type == "Get_Heroes"):
+            pass
+        elif (type == "Hire_Heroes"):
+            pass
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -7,7 +7,11 @@ def test_command_list():
     res = TEST_CLIENT.get(ep.COMMAND_LIST).get_json()
     assert isinstance(res[ep.COMMANDS], list)
 
+test_input = {
+    "Type": "Add_To_Party",
+    "Data": "specific data goes here"
+}
 
 def test_input():
-    res = TEST_CLIENT.get(ep.INPUT).get_json()
+    res = TEST_CLIENT.post(ep.INPUT, json=test_input)
     assert isinstance(res[ep.MESSAGE], str)
