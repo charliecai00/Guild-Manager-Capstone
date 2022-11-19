1c4976c437d0fd952af56bebb1f542f6e66de391
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-19 14:37:10 -0500

    Added Quest generation

--- game/object_classes/quest.py
+++ game/object_classes/quest.py
@@ -1,4 +1,7 @@
 
+from random import randint
+from game.object_classes.challenge import Challenge
+
 class Quest:
     class Node:
         def __init__(self,
@@ -7,11 +10,12 @@ class Quest:
                      stat=None,
                      terminal=False) -> None:
             self.challenge = challenge
-            self.check_stat = stat
+            self.check_stat = stat      # currently not used
             self.children = children
             self.terminal = terminal
             self.parent = None
             self.done = False
+            self.depth = 0
 
         def add_child(self, new_node) -> None:
             new_node.parent = self
@@ -34,15 +38,17 @@ class Quest:
             return result
 
         def __str__(self) -> str:
-            return "|Stat: {}, End?: {}|".format(self.check_stat, self.terminal)
+            return "|Challenge: {}, End?: {}|".format(self.challenge, self.terminal)
 
     # leave default constructor for the MAP
     def __init__(self) -> None:
         self.name = "A Great Quest"
         self.done = False
-        self.root = None    # Quest.Node(Challenge)
-        self.curr_nodes = [self.root]
-        self.nodes_master_list = []
+        self.curr_nodes: list[self.Node] = []
+        self.nodes_master_list: list[self.Node] = []
+        self.root = self.generate_nodes()
+        self.curr_nodes.append(self.root)
+        self.nodes_master_list.append(self.root)
         self.best_p_stats = {}  # local party stats
         self.mean_p_stats = {}  # overriden for each party
 
@@ -52,12 +58,34 @@ class Quest:
 
     def get_info(self) -> list:
         ret_list = [self.name]
-        for c in self.curr_nodes:
+        for c in self.nodes_master_list:
             ret_list.append(str(c))
         return ret_list
 
-    def generate_nodes(self) -> None:
-        pass    # TODO: a system to generate nodes
+    def generate_nodes(self) -> Node:
+        stat_names = ["STR", "CON", "DEX", "WIS", "INT", "CHA"]
+        curr_layer: list[self.Node] = []
+        root = self.Node(Challenge(stat_names[randint(0, len(stat_names)-1)]))
+        self.root = root
+        curr_layer.append(root)
+        max_depth = 5
+        while len(curr_layer) != 0:
+            curr_node: self.Node = curr_layer.pop(0)
+            num_of_children = randint(0,3)
+            if num_of_children != 0 and curr_node.depth < max_depth:
+                for i in range(num_of_children):
+                    new_node = self.Node(Challenge(stat_names[randint(0, len(stat_names)-1)]))
+                    new_node.parent = curr_node
+                    new_node.depth = new_node.parent.depth + 1
+                    curr_node.children.append(new_node)
+                    curr_layer.append(new_node)
+            else:
+                curr_node.terminal = True
+            self.nodes_master_list.append(curr_node)
+        for n in curr_layer:
+            n.terminal = True
+            self.nodes_master_list.append(n)
+        return root
 
     def start(self, best_stats: dict, mean_stats: dict) -> None:
         self.best_p_stats = best_stats.copy()
