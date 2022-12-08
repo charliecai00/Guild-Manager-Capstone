a02d0be8f3f88c856e33981bbc4477050b9c92ff
Charlie Cai <lc4181@nyu.edu>
2022-12-08 16:29:58 -0500

    Update test_game.py

--- db/tests/test_game.py
+++ db/tests/test_game.py
@@ -2,10 +2,8 @@
 # The Guild Manager by JV · CC · ZQ · ZF
 
 import os 
-
 import pytest
-
-import db.game_db as gm
+import db.game as gm
 
 
 TEST_DEL_NAME = 'game to be deleted'
@@ -14,16 +12,16 @@ TEST_DEL_NAME = 'game to be deleted'
 def create_game_details():
     details = {}
     for field in gm.REQUIRED_FLDS:
-        details[field] = 2
+        details[field] = "dummy_data"
     return details
 
 
 @pytest.fixture(scope='function')
 def temp_game():
-    gm.add_game(gm.TEST_GAME_NAME, create_game_details())
+    gm.add_game(gm.TEST_GAME_NAME1, create_game_details())
     yield
-    gm.del_game(gm.TEST_GAME_NAME)
-    
+    gm.del_game(gm.TEST_GAME_NAME1)
+
 
 @pytest.fixture(scope='function')
 def new_game():
@@ -48,34 +46,19 @@ def test_get_games_dict(temp_game):
 
 
 def test_get_game_details(temp_game):
-    gm_dets = gm.get_game_details(gm.TEST_GAME_NAME)
+    gm_dets = gm.get_game_details(gm.TEST_GAME_NAME1)
     assert isinstance(gm_dets, dict)
 
     
 def test_game_exists(temp_game):
-    assert gm.game_exists(gm.TEST_GAME_NAME)
+    assert gm.game_exists(gm.TEST_GAME_NAME1)
 
 
 def test_game_not_exists(temp_game):
     assert not gm.game_exists('Surely this is not a game name!')
 
 
-def test_add_wrong_name_type():
-    with pytest.raises(TypeError):
-        gm.add_game(7, {})
-
-
-def test_add_wrong_details_type():
-    with pytest.raises(TypeError):
-        gm.add_game('a new game', [])
-
-
-def test_add_missing_field():
-    with pytest.raises(ValueError):
-        gm.add_game('a new game', {'foo': 'bar'})
-
-
 def test_add_game():
-    gm.add_game(gm.TEST_GAME_NAME, create_game_details())
-    assert gm.game_exists(gm.TEST_GAME_NAME)
-    gm.del_game(gm.TEST_GAME_NAME)
+    gm.add_game(gm.TEST_GAME_NAME1, create_game_details())
+    assert gm.game_exists(gm.TEST_GAME_NAME1)
+    gm.del_game(gm.TEST_GAME_NAME1)
