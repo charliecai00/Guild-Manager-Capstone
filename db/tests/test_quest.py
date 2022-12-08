704e55e5f018bab3deed7c3d8d27544f16f04b03
Charlie Cai <lc4181@nyu.edu>
2022-12-08 16:51:08 -0500

    update db tests

--- db/tests/test_quest.py
+++ db/tests/test_quest.py
@@ -2,80 +2,63 @@
 # The Guild Manager by JV · CC · ZQ · ZF
 
 import os 
-
 import pytest
-
-import db.challenge as ch
-
-
-TEST_DEL_NAME = 'Challenge to be deleted'
+import db.quest as qs
 
 
-def create_challenge_details():
+TEST_DEL_NAME = 'quest to be deleted'
+
+
+def create_quest_details():
     details = {}
-    for field in ch.REQUIRED_FLDS:
-        details[field] = 2
+    for field in qs.REQUIRED_FLDS:
+        details[field] = "dummy_data"
     return details
 
 
 @pytest.fixture(scope='function')
-def temp_challenge():
-    ch.add_challenge(ch.TEST_CHALLENGE_NAME, create_challenge_details())
+def temp_quest():
+    qs.add_quest(qs.TEST_QUEST, create_quest_details())
     yield
-    ch.del_challenge(ch.TEST_CHALLENGE_NAME)
+    qs.del_quest(qs.TEST_QUEST)
     
 
 @pytest.fixture(scope='function')
-def new_challenge():
-    return ch.add_challenge(TEST_DEL_NAME, create_challenge_details())
+def new_quest():
+    return qs.add_quest(TEST_DEL_NAME, create_quest_details())
 
 
-def test_del_challenge(new_challenge):
-    ch.del_challenge(TEST_DEL_NAME)
-    assert not ch.challenge_exists(TEST_DEL_NAME)
+def test_del_quest(new_quest):
+    qs.del_quest(TEST_DEL_NAME)
+    assert not qs.quest_exists(TEST_DEL_NAME)
 
 
-def test_get_challenges(temp_challenge):
-    chs = ch.get_challenges()
+def test_get_quests(temp_quest):
+    chs = qs.get_quests()
     assert isinstance(chs, list)
     assert len(chs) > 0 #or 1
 
 
-def test_get_challenges_dict(temp_challenge):
-    chs = ch.get_challenges_dict()
+def test_get_quests_dict(temp_quest):
+    chs = qs.get_quests_dict()
     assert isinstance(chs, dict)
     assert len(chs) > 0 #or 1
 
 
-def test_get_challenge_details(temp_challenge):
-    ch_dets = ch.get_challenge_details(ch.TEST_CHALLENGE_NAME)
+def test_get_quest_details(temp_quest):
+    ch_dets = qs.get_quest_details(qs.TEST_QUEST)
     assert isinstance(ch_dets, dict)
 
     
-def test_challenge_exists(temp_challenge):
-    assert ch.challenge_exists(ch.TEST_CHALLENGE_NAME)
+def test_quest_exists(temp_quest):
+    assert qs.quest_exists(qs.TEST_QUEST)
 
 
-def test_challenge_not_exists(temp_challenge):
-    assert not ch.challenge_exists('Surely this is not a challenge name!')
+def test_quest_not_exists(temp_quest):
+    assert not qs.quest_exists('Surely this is not a quest name!')
 
 
-def test_add_wrong_name_type():
-    with pytest.raises(TypeError):
-        ch.add_challenge(7, {})
-
-
-def test_add_wrong_details_type():
-    with pytest.raises(TypeError):
-        ch.add_challenge('a new challenge', [])
-
-
-def test_add_missing_field():
-    with pytest.raises(ValueError):
-        ch.add_challenge('a new challenge', {'foo': 'bar'})
-
-
-def test_add_challenge():
-    ch.add_challenge(ch.TEST_CHALLENGE_NAME, create_challenge_details())
-    assert ch.challenge_exists(ch.TEST_CHALLENGE_NAME)
-    ch.del_challenge(ch.TEST_CHALLENGE_NAME)
+def test_add_quest():
+    qs.add_quest(qs.TEST_QUEST, create_quest_details())
+    assert qs.quest_exists(qs.TEST_QUEST)
+    qs.del_quest(qs.TEST_QUEST)
