befb170d439d6824645988a56e20cf587cad8d9d
Jorge-A-Velasquez <jav494@nyu.edu>
2023-02-11 14:22:09 -0500

    Refactored guild and hero attributes

--- game/object_classes/hero.py
+++ game/object_classes/hero.py
@@ -4,18 +4,16 @@
 from random import randrange
 from game.object_classes.character import Character
 from game.game_math.random import RandomNormalClamped
-import game.object_classes.static_consts as sc
 import db.hero
 
 
 class Hero(Character):
-    def __init__(self, new_id, type=None) -> None:
+    def __init__(self, new_id) -> None:
         Character.__init__(self)
         self.id = new_id
-        self.type = type
-        # which party and guild the hero belongs
+        self.is_hired = False
+        self.is_in_party = False
         self.party_id = None
-        self.guild = None
         self.stats = {
             "STR": RandomNormalClamped(20, 10, 5, 95),
             "CON": RandomNormalClamped(20, 10, 5, 95),
@@ -24,29 +22,30 @@ class Hero(Character):
             "INT": RandomNormalClamped(20, 10, 5, 95),
             "CHA": RandomNormalClamped(20, 10, 5, 95),
         }
-        self.items = []
-        self.name = str(sc.HERO_ID)
-        sc.HERO_ID += 1
-        self.health = 2
+        self.name = str(new_id)     # TODO: name generation
+        self.health = 3
         self.alive = True
         self.cost = 5
-        db.hero.add_hero(self.name, {
+        db.hero.add_hero(self.id, self.as_dict())
+
+    def as_dict(self):
+        return {
             "id": self.id,
-            "stats": self.stats,
-            "items": self.items,
             "name": self.name,
-            "HERO_ID": self.id,
+            "stats": self.stats,
             "health": self.health,
             "alive": self.alive,
-            "cost": self.cost
-        })
+            "cost": self.cost,
+            "hired": self.is_hired,
+            "in_party": self.is_in_party,
+            "pary_id": self.party_id
+        }
 
     def __str__(self) -> str:
-        s = "Name: {}, Health: {}, Stats: {}, Items: {}, Cost: {}\n".format(
+        s = "Name: {}, Health: {}, Stats: {}, Cost: {}\n".format(
             self.name,
             self.health,
             self.stats,
-            self.items,
             self.cost
         )
         return s
@@ -63,3 +62,5 @@ class Hero(Character):
 
     def Take_Damage(self, dmg):
         self.health -= dmg
+        if self.health <= 0:
+            self.alive = False
