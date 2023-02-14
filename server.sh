e4cec3aa758d71d3d8decd05be08e5fc2548f8c7
Charlie Cai <lc4181@nyu.edu>
2023-02-14 17:38:36 -0500

    so why did it work?

--- server.sh
+++ server.sh
@@ -6,4 +6,4 @@
 # run our server locally:
 export PYTHONPATH=$(pwd):$PYTHONPATH
 export FLASK_DEBUG=1
-FLASK_APP=endpoints python3 -m flask run --host=127.0.0.1 --port=8000
+FLASK_APP=server.endpoints python3 -m flask run --host=127.0.0.1 --port=8000
