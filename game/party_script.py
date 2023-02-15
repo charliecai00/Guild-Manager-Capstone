afaba29d5b8c2caf3f4243e7e74db95f8a529389
Jorge-A-Velasquez <jav494@nyu.edu>
2023-02-15 15:05:16 -0500

    added party script

---
+++ game/party_script.py
@@ -0,0 +1,33 @@
+# A NYU Capstone Project
+# The Guild Manager by JV · CC · ZQ · ZF
+
+from random import randint, randrange
+import db.party as db
+
+def generate_party(id, name="TestName"):
+    party_dict = {
+            "ID": 0,
+            "Name": name,
+            "HeroIDs": []
+        }
+    db.add_party(party_dict)
+
+
+def add_party_hero(id, hero_id):
+    pass
+
+
+def remove_party_hero(id, hero_id):
+    pass
+
+
+def test_party_single(id, stat):
+    pass
+
+
+def test_party_team(id, stat):
+    pass
+
+
+def disband_party(id):
+    pass
