383b213d97fb0f6f8ee03e32be685436b673edba
Zhiquan Cao <zc1548@nyu.edu>
2023-02-26 19:30:00 -0500

    improved makefiles

---
+++ common.mk
@@ -0,0 +1,24 @@
+LINTER = flake8
+PYTESTFLAGS = -vv --verbose --tb=short
+
+FORCE:
+
+all_tests: lint unit
+
+lint: FORCE
+		$(LINTER) *.py
+		$(LINTER) tests/*.py
+
+unit: FORCE
+		pytest $(PYTESTFLAGS)
+
+# test a python file:
+%.py: FORCE
+		pytest -s tests/test_$*.py
+
+lint_local:
+		python3 -m $(LINTER) *.py
+		python3 -m $(LINTER) tests/*.py
+
+unit_local:
+		python3 -m pytest $(PYTESTFLAGS)
