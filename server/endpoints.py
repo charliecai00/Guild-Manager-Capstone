3436796d5ca1857afb4477e9bddf1c35e4331400
Zhiquan Cao <zc1548@nyu.edu>
2023-04-08 10:47:25 -0400

    changed the response for sell quest endpoint to comply with the schema

--- server/endpoints.py
+++ server/endpoints.py
@@ -180,7 +180,7 @@ class Sell(Resource):
         flag, msg = quest_script.sell_quest(request.json['id'],
                                             request.json['guild_id'])
         if flag:
-            return {RES: msg}
+            return {RES: "Success"}
         return {RES: msg}
 
 
