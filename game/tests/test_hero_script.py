3219a13c809318d324396b5baf083abace0d5012
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2023-04-16 19:30:02 -0400

    created constants for test_hero_script

--- game/tests/test_hero_script.py
+++ game/tests/test_hero_script.py
@@ -1,5 +1,62 @@
 # A NYU Capstone Project
 # The Guild Manager by JV · CC · ZQ · ZF
 
-# import pytest
-# import hero_script as hs
+from unittest.mock import patch
+import game.hero_script as hs
+
+EX_GUILD_FULL = {
+    "ID": 0,
+    "Name": "TestFull",
+    "HeroIDs": [0, 1, 2, 3, 4],
+    "PartyIDs": [0, 1, 2, 3, 4],
+    "QuestIDs": [0, 1, 2, 3, 4],
+    "Funds": 999,
+    "QuestsCompleted": 999
+}
+EX_GUILD_EMPTY = {
+    "ID": 1,
+    "Name": "TestEmpty",
+    "HeroIDs": [],
+    "PartyIDs": [],
+    "QuestIDs": [],
+    "Funds": 0,
+    "QuestsCompleted": 0
+}
+EX_HERO_HURT = {
+    "ID": 0,
+    "Name": "String",
+    "Health": 1,
+    "MaxHealth": 5,
+    "Exp": 0,
+    "Stats": {
+        "STR": 20,
+        "CON": 20,
+        "DEX": 20,
+        "WIS": 20,
+        "INT": 20,
+        "CHA": 20
+    },
+    "Hired?": False,
+    "InParty?": False,
+    "PartyID": 0,
+    "Cost": 999
+}
+EX_HERO_HEALTHY = {
+    "ID": 0,
+    "Name": "String",
+    "Health": 5,
+    "MaxHealth": 5,
+    "Exp": 0,
+    "Stats": {
+        "STR": 20,
+        "CON": 20,
+        "DEX": 20,
+        "WIS": 20,
+        "INT": 20,
+        "CHA": 20
+    },
+    "Hired?": True,
+    "InParty?": False,
+    "PartyID": 0,
+    "Cost": 999
+}
