128e952e2abfdcc83cfd957880f9ce17943425b9
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2023-04-20 12:35:14 -0400

    implement test_heal_hero_healthy

--- game/tests/test_hero_script.py
+++ game/tests/test_hero_script.py
@@ -85,3 +85,15 @@ def test_heal_hero_broke(get_hero_details_mock,
     ret = hs.heal_hero(0, 0)
     assert ret[1] == "Guild does not have enough funds to heal"
     assert ret[0] is False
+
+@patch('hero_script.hero_db.get_hero_details',
+       return_value=EX_HERO_HEALTHY.copy())
+@patch('hero_script.guild_db.get_guild_details',
+       return_value=EX_GUILD_FULL.copy())
+@patch('hero_script.hero_db.update_hero')
+def test_heal_hero_healthy(get_hero_details_mock,
+                         get_guild_details_mock,
+                         update_hero_mock):
+    ret = hs.heal_hero(0, 0)
+    assert ret[1] == "Hero already healthy"
+    assert ret[0] is False
